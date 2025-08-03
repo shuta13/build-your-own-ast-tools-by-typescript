7c23ac664c2427c90b82b8120a2b09b3f7860467
shuta13 <hollystarsun@gmail.com>
2025-08-03 11:56:59 +0900

    feat: generate code cli

--- 004-generate-code/tsPrinterSample.ts
+++ 004-generate-code/tsPrinterSample.ts
@@ -20,8 +20,8 @@ const ast = f.createVariableStatement(
 );
 
 // ASTをコンソールに出力するためのプリンターを作成します
-const source = ts.createSourceFile('sample.ts', '', ts.ScriptTarget.Latest);
 const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
+const source = ts.createSourceFile('sample.ts', '', ts.ScriptTarget.Latest);
 const code = printer.printNode(ts.EmitHint.Unspecified, ast, source);
 
 console.log(code);
