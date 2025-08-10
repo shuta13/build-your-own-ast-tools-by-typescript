02769241df42d88629ed31fa4572f195388a2de5
shuta13 <hollystarsun@gmail.com>
2025-08-10 17:00:04 +0900

    chore: copied and modified files

---
+++ 004-generate-code/checkAst.ts
@@ -0,0 +1,91 @@
+import type { AstNode } from './createAst.ts';
+import type { Config } from './formatterCli.ts';
+import { debug, error, warn } from './console.ts';
+
+export function checkAst(astNode: AstNode, depth = 0, config: Config) {
+  const indent = '  '.repeat(depth);
+  debug(
+    `${indent}Kind: ${astNode.kind}, Text: ${astNode.text.replaceAll('\n', ' ').slice(0, 20)}`
+  );
+
+  // ルール違反があるかどうかのフラグ
+  let hasViolation = false;
+  // configのcall-super-in-constructorが有効な場合、ルールを適用してASTを検査する
+  if (
+    config['call-super-in-constructor'] !== 'off' &&
+    astNode.kind === 'ClassDeclaration' &&
+    // extendsキーワードが存在する場合、親クラスがあると判断する
+    astNode.children.some((child) => child.kind === 'HeritageClause')
+  ) {
+    // ClassDeclarationのASTノードの中からConstructorのASTノードを探す
+    const constructor = astNode.children.find(
+      (child) => child.kind === 'Constructor'
+    );
+    if (!constructor) return;
+
+    // ConstructorのASTノードの中からBlockのASTノードを探す
+    const block = constructor.children.find((child) => child.kind === 'Block');
+    if (!block) return;
+
+    // BlockのASTノードの中からExpressionStatementのASTノードを探す
+    const expressionStatement = block.children.find(
+      (child) => child.kind === 'ExpressionStatement'
+    );
+    if (!expressionStatement) return;
+
+    // ExpressionStatementのASTノードの中からCallExpressionのASTノードを探す
+    const callExpression = expressionStatement.children.find(
+      (child) => child.kind === 'CallExpression'
+    );
+    // CallExpressionのASTノードが見つからない場合は、super()が呼び出されていないと判断する
+    // また、CallExpressionのtextに'super'が含まれていない場合も同様にする
+    if (!callExpression || !callExpression.text.includes('super')) {
+      const message = 'Constructor should call super() but does not';
+      if (config['call-super-in-constructor'] === 'error') {
+        error(message);
+      }
+      if (config['call-super-in-constructor'] === 'warn') {
+        warn(message);
+      }
+      hasViolation = true;
+    }
+  }
+
+  // configのuse-this-in-methodが有効な場合、ルールを適用してASTを検査する
+  if (
+    config['use-this-in-method'] !== 'off' &&
+    astNode.kind === 'ClassDeclaration'
+  ) {
+    const methodDeclarations = astNode.children.filter(
+      (child) => child.kind === 'MethodDeclaration'
+    );
+    if (methodDeclarations.length === 0) return;
+
+    const blocks = methodDeclarations.flatMap((method) =>
+      method.children.filter((child) => child.kind === 'Block')
+    );
+    if (blocks.length === 0) return;
+
+    const isAllMethodsUsingThis = blocks.every((block) => {
+      return block.text.includes('this.');
+    });
+    if (!isAllMethodsUsingThis) {
+      const message = 'Method should use `this` but does not';
+      if (config['use-this-in-method'] === 'error') {
+        error(message);
+      }
+      if (config['use-this-in-method'] === 'warn') {
+        warn(message);
+      }
+      hasViolation = true;
+    }
+  }
+
+  for (const child of astNode.children) {
+    if (checkAst(child, depth + 1, config)) {
+      hasViolation = true;
+    }
+  }
+
+  return hasViolation;
+}
---
+++ 004-generate-code/formatterCli.ts
@@ -0,0 +1,85 @@
+#!/usr/bin/env node
+
+// Node.jsのモジュールをインポート
+import fs from 'node:fs/promises';
+import path from 'node:path';
+// カスタムコンソールモジュールをインポート
+import { debug, error, info } from './console.ts';
+import { createAst } from './createAst.ts';
+import { checkAst } from './checkAst.ts';
+
+// configの型を定義
+export type Config = {
+  'call-super-in-constructor': 'error' | 'warn' | 'off';
+  'use-this-in-method': 'error' | 'warn' | 'off';
+};
+
+console.log('Welcome to the Formatter CLI!');
+
+const helpMessage = 'Usage: formatter-cli <file-path> [--debug]';
+
+// コマンドの引数を取得（0番目はnodeの実行パス、1番目はスクリプトのパスなので省く）
+const args = process.argv.slice(2);
+// 引数が不足している場合はヘルプメッセージを表示
+if (args.length === 0) {
+  info(helpMessage);
+  process.exit(1);
+}
+
+let [inputFilePath, ...restArgs] = args;
+// コマンドの1つ目の引数に、入力ファイルパスが指定されていない場合はエラーにする
+if (!inputFilePath || inputFilePath.startsWith('--')) {
+  error('Input file path is required');
+  info(helpMessage);
+  process.exit(1);
+}
+
+// コマンドが実行されたディレクトリにconfigファイルがない場合はエラーにする
+const configFilePath = path.resolve('config.json');
+const isConfigFileExists = await fs.access(configFilePath).then(() => true).catch(() => false);
+if (!isConfigFileExists) {
+  error('Config file not found: config.json');
+  process.exit(1);
+}
+
+for (let i = 0; i < restArgs.length; i++) {
+  const arg = restArgs[i];
+
+  if (arg === '--debug') {
+    // --debugオプションが指定された場合、デバッグモードを有効にする
+    process.env.DEBUG = 'true';
+  } else {
+    error(`Unknown argument: ${arg}`);
+    process.exit(1);
+  }
+}
+
+// 入力ファイルを読み込む
+try {
+  const absoluteInputPath = path.resolve(inputFilePath);
+  const data = (await fs.readFile(absoluteInputPath)).toString();
+
+  debug(`Input file path: ${absoluteInputPath}`);
+  debug(`Input file content:\n${data}`);
+
+  // configファイルを読み込む
+  const config: Config = JSON.parse(
+    (await fs.readFile(configFilePath)).toString()
+  );
+  debug(`Config file content:\n${JSON.stringify(config, null, 2)}`);
+
+  const ast = await createAst(absoluteInputPath);
+
+  // AST を走査して情報を出力する
+  const violated = checkAst(ast, 0, config);
+  if (!violated) {
+    info('No issues found');
+  }
+} catch (err) {
+  if (err instanceof Error) {
+    error(err.message);
+  } else {
+    error('An unknown error occurred');
+  }
+  process.exit(1);
+}
